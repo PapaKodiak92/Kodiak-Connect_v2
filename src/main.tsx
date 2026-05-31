22b5a607d0313c8eb107b1054646a9da6d95f9cf
PapaKodiak92 <brisendine.joseph92@gmail.com>
2026-05-30 23:41:41 -0500

    feat: load compact launcher stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 import './styles/global.css';
 import './styles/brand.css';
+import './styles/launcher.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
