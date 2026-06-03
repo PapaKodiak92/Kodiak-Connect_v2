8e6512287198aa443be11fa5ee8700dd73267e9c
PapaKodiak <brisendine.joseph92@gmail.com>
2026-06-02 23:42:58 -0500

    chore: prepare 1.1.8 release

--- src/features/updater/updateManifest.ts
+++ src/features/updater/updateManifest.ts
@@ -16,7 +16,7 @@ export interface KodiakUpdateManifest {
 
 export const updateManifest: KodiakUpdateManifest = {
   appId: 'com.kodiakholdings.kodiakconnect',
-  currentVersion: '1.1.7',
+  currentVersion: '1.1.8',
   channel: 'dev',
   generatedAt: '2026-06-02T19:45:49.315Z',
   targets: [],
