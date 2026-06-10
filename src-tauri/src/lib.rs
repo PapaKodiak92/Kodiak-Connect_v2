1cf8298c7aeae6b8f3b5a855cd7b0065ef008396
PapaKodiak <brisendine.joseph92@gmail.com>
2026-06-10 11:48:25 -0500

    Add notification dependencies and remove dev update workflow

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -91,6 +91,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             choose_save_path,
